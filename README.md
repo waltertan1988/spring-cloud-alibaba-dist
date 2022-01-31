7142e749b43eebf64addeb7fe35c7eaf12794974
tanyunxing <tanyunxing@yy.com>
2022-01-31 14:36:52 +0800

    整合nacos

--- README.md
+++ README.md
@@ -1,6 +1,12 @@
 # spring-cloud-alibaba-dist
 spring-cloud-alibaba学习
 
-## 版本说明
+### 版本说明
 * 使用的Spring Cloud Alibaba的版本为2.2.6.RELEASE
 * Spring Cloud Alibaba各组件对应的版本，参看[这里](https://github.com/alibaba/spring-cloud-alibaba/wiki/%E7%89%88%E6%9C%AC%E8%AF%B4%E6%98%8E)
+
+###启动nacos，默认账号密码都是nacos
+```text
+startup.cmd -m standalone
+```
+          
--- spring-cloud-alibaba-dubbo-provider/src/main/java/com/walter/dubbo/DubboApplication.java
+++ spring-cloud-alibaba-dubbo-provider/src/main/java/com/walter/dubbo/DubboApplication.java
@@ -3,9 +3,11 @@ package com.walter.dubbo;
 import org.apache.dubbo.config.spring.context.annotation.DubboComponentScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @DubboComponentScan
 @SpringBootApplication
+@EnableDiscoveryClient
 public class DubboApplication {
 	
 	public static void main(String[] args) {
