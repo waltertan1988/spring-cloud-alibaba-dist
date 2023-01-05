a83a0c765ed43ef48846fab5c33af20ac8f18175
walter.tan <Walter.Tan@moco.com>
2023-01-05 11:23:39 +0800

    调整pom

--- spring-cloud-alibaba-dubbo-consumer/src/main/java/com/walter/mock/HelloApiMock.java
+++ spring-cloud-alibaba-dubbo-consumer/src/main/java/com/walter/mock/HelloApiMock.java
@@ -1,17 +1,21 @@
 package com.walter.mock;
 
+import com.alibaba.fastjson2.JSON;
 import com.walter.dubbo.api.HelloApi;
 import com.walter.dubbo.vo.SayHelloRequest;
+import lombok.extern.slf4j.Slf4j;
 
 /**
  * @author think
  * @date 2022/1/29
  *
  */
+@Slf4j
 public class HelloApiMock implements HelloApi {
 
     @Override
     public String say(SayHelloRequest request) {
-        return "无法调用com.walter.dubbo.api.HelloApi.say，服务降级中，请稍后再试...";
+        log.info("say mock is invoked: {}", JSON.toJSONString(request));
+        return "无法调用com.walter.dubbo.api.HelloApi.say，服务通过Mock方式降级中，请稍后再试...";
     }
 }
--- spring-cloud-alibaba-dubbo-provider/src/main/java/com/walter/dubbo/api/HelloApiImpl.java
+++ spring-cloud-alibaba-dubbo-provider/src/main/java/com/walter/dubbo/api/HelloApiImpl.java
@@ -1,5 +1,6 @@
 package com.walter.dubbo.api;
 
+import com.alibaba.fastjson2.JSON;
 import com.walter.dubbo.vo.SayHelloRequest;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.dubbo.config.annotation.DubboService;
@@ -15,6 +16,7 @@ public class HelloApiImpl implements HelloApi {
 
     @Override
     public String say(SayHelloRequest request) {
+        log.info("say is invoked: {}", JSON.toJSONString(request));
         return String.format("%s says hello to %s", request.getFrom(), request.getTo());
     }
 }
